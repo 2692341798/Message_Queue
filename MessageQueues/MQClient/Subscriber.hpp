9b46211a2c72fd8eff50d3f6fbfcb03f2386bd22
ZZJM <2692341798@qq.com>
2025-05-07 11:16:00 +0800

    客户端-订阅者模块的编写

---
+++ MessageQueues/MQClient/Subscriber.hpp
@@ -0,0 +1,44 @@
+#ifndef __M_Subscriber_H__
+#define __M_Subscriber_H__
+#include "../MQCommon/Helper.hpp"
+#include "../MQCommon/Logger.hpp"
+#include "../MQCommon/message.pb.h"
+#include <google/protobuf/map.h>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <unordered_map>
+
+namespace MQ 
+{
+   using SubscriberCallback = std::function<void(const std::string, const BasicProperties *bp, const std::string)>;
+
+  struct Subscriber
+  {
+    // 自动应答标志
+    bool _auto_ack;
+    // 订阅的队列名称
+    std::string _subscribe_queue_name;
+    // 消费者标识
+    std::string _consumer_tag;
+    // 消费者回调函数
+    SubscriberCallback _callback;
+
+    // 指针
+    using ptr = std::shared_ptr<Subscriber>;
+
+    // 构造函数
+    Subscriber() {}
+
+    Subscriber(const std::string &consumer_tag, const std::string &subscribe_queue_name, bool auto_ack, const ConsumerCallback &callback)
+        : _auto_ack(auto_ack),
+          _subscribe_queue_name(subscribe_queue_name),
+          _consumer_tag(consumer_tag),
+          _callback(callback)
+    {}
+    // 析构函数
+    virtual ~Subscriber() {}
+  };
+
+}
+#endif
